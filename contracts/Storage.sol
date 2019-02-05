d93a2754c70eda0263243c371696d9d8ee7ec484
ajaysethi8789 <46188133+ajaysethi8789@users.noreply.github.com>
2019-02-05 18:55:17 +0530

    Update Storage.sol

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.5.1;
 contract Storage {
   uint256 storedData;
-function set(uint256 data) public {
+function set(uint256 data)  public {
     storedData = data;
   }
-function get() view returns (uint256) public {
+function get() public view returns (uint256)  {
     return storedData;
   }
 }
