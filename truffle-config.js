b93efaca5a8c9ff15d1524d12d533c9e3dc93e3c
ajaysethi8789 <46188133+ajaysethi8789@users.noreply.github.com>
2019-03-01 14:50:29 +0530

    update truffle config.js

--- truffle-config.js
+++ truffle-config.js
@@ -2,9 +2,15 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 8545, 
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+            test: {
+            host: "13.127.208.114",
+            port: 8545,
             network_id: "*" // Match any network id
         }
+
     }
-    
+
 };
